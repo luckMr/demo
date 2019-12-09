193181f422d22b8b6271f0898ae9c513d425082a
luckMr <2936698831@qq.com>
2019-12-09 22:05:10 +0800

    helloword

---
+++ HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWord {
+	public static void main(String[] args){
+		System.out.println("HelloWord");
+		System.out.println("HelloWord2");
+		System.out.println("HelloWord3");
+		System.out.println("HelloWord4");
+	}
+}
